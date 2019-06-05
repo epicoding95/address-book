0e5725d9046c30eb9dec19a83b2e76aaaab2ee7e
kelar crisp and Lucas Blasdel <kelar@>
2019-06-05 16:53:00 -0700

    beginning of address-book

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("form#addressinfo").submit(function(event) {
+    event.preventDefault()
+    var nameResponse=($("#name1").val());
+    var addressResponse=($("#address1").val());
+    var numResponse=($("#number1").val());
+
+  });
+});
